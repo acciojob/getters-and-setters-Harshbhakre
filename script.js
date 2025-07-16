b7019a2c231826dfa2ad07ddea54d34139737eaa
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-16 13:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ class Student extends Person {
         super(name,age)
     }
     study(){
-        console.log(this.name + " is studing");
+        console.log(this.name + " is studying");
     }
 }
 
@@ -37,21 +37,6 @@ class Teacher extends Person {
         console.log(this.name + " is teaching")
     }
 }
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
-
-person.age = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
-
 
 // Do not change the code below this line
 window.Person = Person;
